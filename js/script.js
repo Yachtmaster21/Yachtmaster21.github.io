9eb090cd53e0a4032ae663bc8c435aeb5388c351
Yachtmaster21 <66702278+Yachtmaster21@users.noreply.github.com>
2020-09-18 22:43:19 -0400

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 
 
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-about-finn","displayName":"About Finn","link":{"linkType":"LinkTypePage","href":"#!page-about-finn"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true}]}
+
         $(function () {
             $('.go-down').click(function () {
                 $('html,body').animate({scrollTop: $('#after-hero').offset().top}, 500);
